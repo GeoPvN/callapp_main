be600d9b24c11a5317dafc2f7ca42aee0f584b51
datopapala <dpapalashvili@yahoo.com>
2015-11-18 14:20:35 +0400

    sdfs

--- includes/phpmailer/smtp.php
+++ includes/phpmailer/smtp.php
@@ -32,7 +32,7 @@ $mail->SMTPDebug = 2;
 //Ask for HTML-friendly debug output
 $mail->Debugoutput = 'html';
 //Set the hostname of the mail server
-$mail->Host = "mail.carekids.ge";
+$mail->Host = "carekids.ge";
 //Set the SMTP port number - likely to be 25, 465 or 587
 $mail->Port = 25;
 //Whether to use SMTP authentication
