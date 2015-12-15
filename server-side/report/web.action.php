6e148c3d8e176ae2faaba6df64114e3defa5c53b
adm <adm@adm-PC>
2015-12-15 14:31:26 +0400

    rtyrty

--- server-side/report/web.action.php
+++ server-side/report/web.action.php
@@ -40,7 +40,7 @@ switch ($action) {
 		    $filt         = "AND asterisk_outgoing.extension='$agent'";
 		    $filt_user    = "AND sent_mail.user_id='$agent_user_id'";
 		    $filt_agent   = "AND access_log.agent='$agent_filt'";
-		    $click_agent  = "WHERE click.agent='$agent_filt'";
+		    $click_agent  = "AND click_log.agent='$agent_filt'";
 		}
 		$rResult = mysql_query("SELECT  access_log.id,
 		                                (SELECT COUNT(asterisk_outgoing.phone) FROM `asterisk_outgoing`
