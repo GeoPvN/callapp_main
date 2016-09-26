187c8bd0247d725b67a31fdf92667542bb8fd1df
datopapala <dpapalashvili@yahoo.com>
2016-09-26 11:57:00 +0400

    f

--- client-side/call/incomming.php
+++ client-side/call/incomming.php
@@ -497,25 +497,30 @@
     }
 
     function SaveToDisk(fileURL, fileName) {
-        // for non-IE
-        if (!window.ActiveXObject) {
-            var save = document.createElement('a');
-            save.href = fileURL;
-            save.target = '_blank';
-            save.download = fileName || 'unknown';
+//         // for non-IE
+//         if (!window.ActiveXObject) {
+//             var save = document.createElement('a');
+//             save.href = fileURL;
+//             save.target = '_blank';
+//             save.download = fileName || 'unknown';
 
-            var event = document.createEvent('Event');
-            event.initEvent('click', true, true);
-            save.dispatchEvent(event);
-            (window.URL || window.webkitURL).revokeObjectURL(save.href);
-        }
-	     // for IE
-        else if ( !! window.ActiveXObject && document.execCommand)     {
-            var _window = window.open(fileURL, "_blank");
-            _window.document.close();
-            _window.document.execCommand('SaveAs', true, fileName || fileURL)
-            _window.close();
-        }
+//             var event = document.createEvent('Event');
+//             event.initEvent('click', true, true);
+//             save.dispatchEvent(event);
+//             (window.URL || window.webkitURL).revokeObjectURL(save.href);
+//         }
+// 	     // for IE
+//         else if ( !! window.ActiveXObject && document.execCommand)     {
+//             var _window = window.open(fileURL, "_blank");
+//             _window.document.close();
+//             _window.document.execCommand('SaveAs', true, fileName || fileURL)
+//             _window.close();
+//         }
+    	var iframe = document.createElement("iframe"); 
+        iframe.src = fileURL; 
+        iframe.style.display = "none"; 
+        document.body.appendChild(iframe);
+        return false;
     }
 
     $(document).on("click", "#send_sms", function (fName) {
