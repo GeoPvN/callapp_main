8ccdb5eed0137ebbd0cf53294f77160b40d50f09
datopapala <dpapalashvili@yahoo.com>
2016-09-27 11:55:06 +0400

    fd

--- server-side/info/template_actived.action.php
+++ server-side/info/template_actived.action.php
@@ -116,7 +116,7 @@ function GetPage(){
 	$data  .= '
 	
 	<div id="dialog-form">
-	    <fieldset style="width: 100%;">
+	    <fieldset style="width: 90%;">
 	       <legend>ძირითადი ინფორმაცია</legend>
 		   <label for="actived_number">რაოდენობა</label>
 	       <input type="number" id="actived_number" min="1">
