cb27aa367d0d552509c40722599cabb6fcddd904
datopapala <dpapalashvili@yahoo.com>
2016-10-24 14:13:22 +0400

    d

--- AsteriskManager/liveStatemini.php
+++ AsteriskManager/liveStatemini.php
@@ -6,9 +6,7 @@ $time = $time[1] + $time[0];
 $begintime = $time;
 $inuse      = Array();
 $dict_queue = Array();
-$qu = $_REQUEST['qu'];
-$filter_queues = array($qu);
-
+$filter_queues = array("2473232","2500055","2182414");
 require("config.php");
 require("asmanager.php");
 require("realtime_functions.php");
@@ -22,22 +20,17 @@ if(isset($_SESSION['QSTATS']['filter'])) {
 } else {
     $filter="";
 }
-
 $am=new AsteriskManager();
 $am->connect($manager_host,$manager_user,$manager_secret);
-
 $channels = get_channels ($am);
 foreach($channels as $ch=>$chv) {
   list($chan,$ses) = split("-",$ch,2);
   $inuse["$chan"]=$ch;
 }
-
 $queues   = get_queues   ($am,$channels);
-
 foreach ($queues as $key=>$val) {
   $queue[] = $key;
 }
-
 $color['unavailable']="flesh_off.png";
 $color['unknown']="#dadada";
 $color['busy']="flesh_inc.png";
@@ -45,40 +38,77 @@ $color['dialout']="#d0303f";
 $color['ringing']="flesh_ringing.png";
 $color['not in use']="flesh_free.png";
 $color['paused']="#000000";
-
-
-$off=0;
-$free=0;
-$use=0;
 foreach($filter_queues  as $qn) {
 	if($filter=="" || stristr($qn,$filter)) {
 		$contador=1;
 		if(!isset($queues[$qn]['members'])) continue;
 		foreach($queues[$qn]['members'] as $key=>$val) {
+			 
+			$stat="";
+			$last="";
+			$dur="";
+			$clid="";
+			$akey = $queues[$qn]['members'][$key]['agent'];
+			$aname = $queues[$qn]['members'][$key]['name'];
+			$aval = $queues[$qn]['members'][$key]['type'];
+			if(array_key_exists($key,$inuse)) {
+				if($aval=="not in use") {
+					$aval = "dialout";
+				}
+				if($channels[$inuse[$key]]['duration']=='') {
+					$newkey = $channels[$inuse[$key]]['bridgedto'];
+					$dur = $channels[$newkey]['duration_str'];
+					$clid = $channels[$newkey]['callerid'];
+				} else {
+					$newkey = $channels[$inuse[$key]]['bridgedto'];
+					$clid = $channels[$newkey]['callerid'];
+					$dur = $channels[$inuse[$key]]['duration_str'];
+				}
+			}
+			$stat = $queues[$qn]['members'][$key]['status'];
+			$last = $queues[$qn]['members'][$key]['lastcall'];
+			if(($aval == "unavailable" || $aval == "unknown") && $ocultar=="true") {
+				// Skip
+			} else {
+				if($contador==1) {
+					echo '<tr>
+                            <td colspan="6" style="border-left: 1px solid #E6E6E6;border-right: 1px solid #E6E6E6;">სთეიშენები</td>
+                          </tr>
+					      <tr class="tb_head" style="border: 1px solid #E6E6E6;">
+                            <td style="width:75px">შიდა ნომერი</td>
+                            <td style="width:50px">სტატუსი</td>
+                          </tr>';
+				}
+				if($contador%2) {
+					$odd="class='odd'";
+				} else {
+					$odd="";
+				}
+				if($last<>"") {
+					$last=$last." ".$lang[$language]['min_ago'];
+				} else {
+					$last = $lang[$language]['no_info'];
+				}
+				$agent_name = agent_name($aname);
+				echo '<tr style="border: 1px solid #E6E6E6;">';
+				echo "<td>$agent_name</td>";
+				if($stat<>"") {
+				$aval="paused";
+			}
+			if(!array_key_exists($key,$inuse)) {
+					if($aval=="busy") $aval="not in use";
+			}
+			$aval2 = ereg_replace(" ","_",$aval);
+			$mystringaval = $lang[$language][$aval2];
+			if($mystringaval=="") $mystringaval = $aval;
+			echo '<td class="td_center"><img alt="inner" src="media/images/icons/'.$color[$aval].'" height="14" width="14"></td>';	
+			echo "</tr>";
+			$contador++;
+			}
+			}
+		if($contador>1) {
 		
-		    $stat="";
-		    $last="";
-		    $dur="";
-		    $clid="";
-		    $akey = $queues[$qn]['members'][$key]['agent'];
-		    $aname = $queues[$qn]['members'][$key]['name'];
-		    $aval = $queues[$qn]['members'][$key]['type'];
-		    $stat = $queues[$qn]['members'][$key]['status'];
-		    $last = $queues[$qn]['members'][$key]['lastcall'];
-		
-		    
-		    if($aval == "unavailable" || $aval == "unknown") {
-		        $off++;
-		    } else if($aval == "busy") {
-		        $use++;
-		        
-		    } else {
-		        $free++;
-		    }
 		}
-
 	}
 }
-$data = array('off'=>$off,'free'=>$free,'use'=>$use);
-echo json_encode($data, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 ?>
