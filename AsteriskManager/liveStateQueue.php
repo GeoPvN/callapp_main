ee470965e04298a1028ee5cc13bfd8d1530beaa0
datopapala <dpapalashvili@yahoo.com>
2016-10-26 15:05:58 +0400

    fdf

--- AsteriskManager/liveStateQueue.php
+++ AsteriskManager/liveStateQueue.php
@@ -6,7 +6,7 @@ $time = $time[1] + $time[0];
 $begintime = $time;
 $inuse      = Array();
 $dict_queue = Array();
-$filter_queues = array("2473232","2500055","022028");
+$filter_queues = array("2473232","2500055","2022028");
 
 require("config.php");
 require("asmanager.php");
